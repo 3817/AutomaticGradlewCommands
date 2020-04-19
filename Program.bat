67cbc65e39eed5f7a0ec3eab2c6781d1dad50ca0
3817 <63802735+3817@users.noreply.github.com>
2020-04-19 19:44:46 +0100

    Create Program.bat

---
+++ Program.bat
@@ -0,0 +1,15 @@
+title automatic gradlew commands by norkenbot
+echo off
+color 04
+cls
+dir
+echo Hey User welcome to NorkenBot´s Automatic Gradlew command executer.
+echo before you use this you must have Java 8 and gradlew installed.
+echo if you dont have this installed download java from the website and execute gradlew.bat
+echo then execute this.
+echo your ready?
+echo lets do it!
+pause
+gradlew.bat build
+pause
+echo Congrats on compiling your mod!!
